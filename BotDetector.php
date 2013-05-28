2b9283754b394e4bbb569aa90220d6d9e032404f
Theodore Brown <theodorejb@outlook.com>
2013-05-27 20:03:51 -0500

    insertToken() should accept Ajax resp. file URL
    Fixes #1

--- BotDetector.php
+++ BotDetector.php
@@ -1,26 +1,31 @@
 <?php
 
 /**
- * BotDetector prevents spam form submissions by requiring a unique token, fetched with Ajax, to be submitted via a hidden field (requires JavaScript to be enabled)
+ * BotDetector prevents spam form submissions by requiring a unique token, fetched 
+ * with Ajax, to be submitted via a hidden field (requires JavaScript to be enabled)
  * 
- * Currently depends on jQuery, and only supports forms submitted via POST
+ * BotDetector Currently depends on jQuery, and only supports forms submitted via POST
  * 
  * Usage:
  * 
- * 1. Change the $secretKey property to a secret string of your own choosing (preferably something random that won't be easily guessed)
- * 2. Include BotDetector.php on the page you want to protect and initialize the BotDetector class
- * 3. After your form, call the insertToken() method and pass it the form ID and relative path to the BotDetector folder
+ * 1. Change the $secretKey property to a string of your own choosing (preferably 
+ *    something random that won't be easily guessed)
+ * 2. Include BotDetector.php and initialize the BotDetector class
+ * 3. After your form, call the insertToken() method and pass it the form ID and 
+ *    relative path to BotDetectorAjax.php
  * 4. When the form is submitted, call the validate() method within a try...catch block
  * 
  * How it works:
  * 
  * 1. When the form is requested, the current timestamp is stored a session variable.
- * 2. When the key is requested via Ajax, BotDetector returns the timestamp, salted & hashed (difficult to fake)
+ * 2. When the key is requested via Ajax, BotDetector returns this timestamp, 
+ *    salted & hashed (difficult to fake)
  * 3. JavaScript inserts the key into a hidden input field in the form
- * 4. When the form is submitted, if the key is not present or does not match the timestamp in the session, validation will fail.
+ * 4. When the form is submitted, if the key is not present or does not match the 
+ *    timestamp in the session, validation will fail.
  *
  * @author Theodore Brown
- * @version 2013.03.25
+ * @version 2013.05.27
  */
 class BotDetector {
 
@@ -51,13 +56,13 @@ class BotDetector {
      * Runs an Ajax script to fetch the key and embed it in the form
      * 
      * @param String $formId the ID of the form to validate
-     * @param String $pathToBotDetector A relative path from the form page to the BotDetector folder
+     * @param String $pathToAjaxResponseFile A relative path from the form page to BotDetectorAjax.php
      */
-    public function insertToken($formId, $pathToBotDetector) {
+    public function insertToken($formId, $pathToAjaxResponseFile) {
         echo <<<_SCRIPT
         <script>
             $.ajax({
-                url: "$pathToBotDetector/BotDetectorAjax.php",
+                url: "$pathToAjaxResponseFile",
                 type: "POST",
                 dataType: "text",
                 success: function(data) {
